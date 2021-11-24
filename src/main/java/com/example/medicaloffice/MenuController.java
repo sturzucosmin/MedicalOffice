b7ed111fe0f0bc22e52ae558464f7ebfd479ec50
sturzucosmin <sturzu.cosmin2222@gmail.com>
2021-11-24 23:36:55 +0200

    Initial commit

---
+++ src/main/java/com/example/medicaloffice/MenuController.java
@@ -0,0 +1,17 @@
+package com.example.medicaloffice;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.PasswordField;
+
+import javafx.stage.Stage;
+
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+
+public class MenuController {
+}
